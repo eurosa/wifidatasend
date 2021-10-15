da1f85442cf2182d4454681881690f41052ff5e8
eurosa <radhasonk.phy@gmail.com>
2021-10-15 16:41:43 -0700

    Add files via upload

---
+++ wifi_config.ino
@@ -0,0 +1,409 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+#include <WiFiClient.h>
+#include <ArduinoJson.h>
+#include <Bridge.h>
+#include <HttpClient.h>
+#define pin 5
+ 
+//Variables
+int i = 0;
+int statusCode;
+const char* ssid = "text";
+const char* passphrase = "text";
+String st;
+String content;
+ 
+ 
+//Function Decalration
+bool testWifi(void);
+void launchWeb(void);
+void setupAP(void);
+ const char* host = "timxn.com"; // only google.com not https://google.com
+ char url[] = "www.timxn.com"; // This could also be 192.168.1.18/~me if you are running a server on your computer on a local network.
+
+// This is the data that will be passed into your POST and matches your mysql column
+int yourarduinodata = 999;
+String yourdatacolumn = "name=";
+String yourdata;
+//Establishing Local server at port 80 whenever required
+ESP8266WebServer server(80);
+ 
+void setup()
+{
+ 
+  Serial.begin(115200); //Initialising if(DEBUG)Serial Monitor
+  Serial.println();
+  Serial.println("Disconnecting previously connected WiFi");
+  WiFi.disconnect();
+  EEPROM.begin(512); //Initialasing EEPROM
+  delay(10);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println();
+  Serial.println();
+  Serial.println("Startup");
+ 
+  //---------------------------------------- Read EEPROM for SSID and pass
+  Serial.println("Reading EEPROM ssid");
+ 
+  String esid;
+  for (int i = 0; i < 32; ++i)
+  {
+    esid += char(EEPROM.read(i));
+  }
+  Serial.println();
+  Serial.print("SSID: ");
+  Serial.println(esid);
+  Serial.println("Reading EEPROM pass");
+ 
+  String epass = "";
+  for (int i = 32; i < 96; ++i)
+  {
+    epass += char(EEPROM.read(i));
+  }
+  Serial.print("PASS: ");
+  Serial.println(epass);
+ 
+ 
+  WiFi.begin(esid.c_str(), epass.c_str());
+  if (testWifi())
+  {
+    Serial.println("Succesfully Connected!!!");
+    return;
+  }
+  else
+  {
+    Serial.println("Turning the HotSpot On");
+    launchWeb();
+    setupAP();// Setup HotSpot
+  }
+ 
+  Serial.println();
+  Serial.println("Waiting.");
+  
+  while ((WiFi.status() != WL_CONNECTED))
+  {
+    Serial.print(".");
+    delay(100);
+    server.handleClient();
+  }
+ 
+}
+void loop() {
+  if ((WiFi.status() == WL_CONNECTED))
+  {
+ 
+    for (int i = 0; i < 2; i++)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(1000);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(1000);
+    }
+   
+    
+//postData();
+ delay(5000);
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClientSecure client;
+  const int httpPort = 443; // 80 is for HTTP / 443 is for HTTPS!
+  
+  client.setInsecure(); // this is the magical line that makes everything work
+  
+  if (!client.connect(host, httpPort)) { //works!
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/ecom/saveName.php";
+  url += "?name=HIGH";
+ // url += "aaaa";
+
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.0\r\n" +
+               "Host: " + host + "\r\n" + 
+               "User-Agent: BuildFailureDetectorESP8266\r\n" +
+               "Connection: close\r\n\r\n");
+
+  /*client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "User-Agent: BuildFailureDetectorESP8266\r\n" +
+                 "Connection: close\r\n\r\n");*/
+
+    // JSON
+    // Allocate the JSON document
+    const size_t capacity = JSON_ARRAY_SIZE(10) + 10 * JSON_OBJECT_SIZE(2) + 10 * JSON_OBJECT_SIZE(3) + 10 * JSON_OBJECT_SIZE(5) + 10 * JSON_OBJECT_SIZE(8) + 3730;
+    DynamicJsonDocument doc(capacity);
+
+     HTTPClient http;
+
+      // Send request
+      http.begin(client,"https://timxn.com/ecom/getiotdata.php");
+      http.GET();
+    // char* str = "{\"status\":\"true\",\"name\":\"HIGH\"}";
+       // Print the response
+      Serial.print("json: "+http.getString());
+
+ 
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, http.getString());
+
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+
+  const char* sensor = doc["name"];
+ 
+
+  // Print values.
+  Serial.println(sensor);
+  Serial.println();
+  Serial.println("closing connection");
+    
+  }
+  else
+  {
+  }
+ 
+}
+
+/*
+void getData() {  
+  HTTPClient http;
+  WiFiClient client;
+  http.begin(client,"http.timxn.com/ecom/getiotdata.php");
+
+ // Serial.print("Request Link:");
+  //Serial.println(host);
+  
+  //http.begin(host);     //Specify request destination
+  
+  int httpCode = http.GET();            //Send the request
+  String payload = http.getString();    //Get the response payload from server
+
+  Serial.print("Response Code:"); //200 is OK
+  Serial.println(httpCode);   //Print HTTP return code
+
+  Serial.print("Returned data from Server:");
+  Serial.println(payload);    //Print request response payload
+  
+  if(httpCode == 200)
+  {
+    // Allocate JsonBuffer
+    // Use arduinojson.org/assistant to compute the capacity.
+    const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
+    DynamicJsonBuffer jsonBuffer(capacity);
+  
+   // Parse JSON object
+    JsonObject& root = jsonBuffer.parseObject(payload);
+    if (!root.success()) {
+      Serial.println(F("Parsing failed!"));
+      return;
+    }
+  
+    // Decode JSON/Extract values
+    Serial.println(F("Response:"));
+    Serial.println(root["name"].as<char*>());
+   // Serial.println(root["time"].as<char*>());
+   // Serial.println(root["data"][0].as<char*>());
+   // Serial.println(root["data"][1].as<char*>());
+  }
+  else
+  {
+    Serial.println("Error in response");
+  }
+
+  http.end();  //Close connection
+  
+  delay(5000);  //GET Data at every 5 seconds
+}
+*/
+
+
+
+
+// This method makes a HTTP connection to the server and POSTs data
+void postData() {
+  WiFiClient client;
+   HTTPClient http;    //Declare object of class HTTPClient
+
+   http.begin(client,"https://timxn.com/ecom/saveName.php");      //Specify request destination
+   http.addHeader("Content-Type", "application/x-www-form-urlencoded");  //  //Specify content-type header
+    String httpRequestData = "name=" + String(random(40));
+//String httpRequestData = "{\"name\":\"" + String(random(40)) + "\"}"; 
+   int httpCode = http.POST(httpRequestData);   //Send the request
+   String payload = http.getString();                  //Get the response payload
+
+   Serial.println(httpCode);   //Print HTTP return code
+   Serial.println(payload);    //Print request response payload
+
+   http.end();  //Close connection
+  
+
+}
+ 
+ 
+//-------- Fuctions used for WiFi credentials saving and connecting to it which you do not need to change 
+bool testWifi(void)
+{
+  int c = 0;
+  Serial.println("Waiting for Wifi to connect");
+  while ( c < 20 ) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      return true;
+    }
+    delay(500);
+    Serial.print("*");
+    c++;
+  }
+  Serial.println("");
+  Serial.println("Connect timed out, opening AP");
+  return false;
+}
+ 
+void launchWeb()
+{
+  Serial.println("");
+  if (WiFi.status() == WL_CONNECTED)
+    Serial.println("WiFi connected");
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("SoftAP IP: ");
+  Serial.println(WiFi.softAPIP());
+  createWebServer();
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+}
+ 
+void setupAP(void)
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*");
+      delay(10);
+    }
+  }
+  Serial.println("");
+  st = "<ol>";
+  for (int i = 0; i < n; ++i)
+  {
+    // Print SSID and RSSI for each network found
+    st += "<li>";
+    st += WiFi.SSID(i);
+    st += " (";
+    st += WiFi.RSSI(i);
+ 
+    st += ")";
+    st += (WiFi.encryptionType(i) == ENC_TYPE_NONE) ? " " : "*";
+    st += "</li>";
+  }
+  st += "</ol>";
+  delay(100);
+  WiFi.softAP("how2electronics", "");
+  Serial.println("softap");
+  launchWeb();
+  Serial.println("over");
+}
+ 
+void createWebServer()
+{
+ {
+    server.on("/", []() {
+ 
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+      content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+      content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
+      content += ipStr;
+      content += "<p>";
+      content += st;
+      content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+      content += "</html>";
+      server.send(200, "text/html", content);
+    });
+    server.on("/scan", []() {
+      //setupAP();
+      IPAddress ip = WiFi.softAPIP();
+      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+ 
+      content = "<!DOCTYPE HTML>\r\n<html>go back";
+      server.send(200, "text/html", content);
+    });
+ 
+    server.on("/setting", []() {
+      String qsid = server.arg("ssid");
+      String qpass = server.arg("pass");
+      if (qsid.length() > 0 && qpass.length() > 0) {
+        Serial.println("clearing eeprom");
+        for (int i = 0; i < 96; ++i) {
+          EEPROM.write(i, 0);
+        }
+        Serial.println(qsid);
+        Serial.println("");
+        Serial.println(qpass);
+        Serial.println("");
+ 
+        Serial.println("writing eeprom ssid:");
+        for (int i = 0; i < qsid.length(); ++i)
+        {
+          EEPROM.write(i, qsid[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qsid[i]);
+        }
+        Serial.println("writing eeprom pass:");
+        for (int i = 0; i < qpass.length(); ++i)
+        {
+          EEPROM.write(32 + i, qpass[i]);
+          Serial.print("Wrote: ");
+          Serial.println(qpass[i]);
+        }
+        EEPROM.commit();
+ 
+        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+        statusCode = 200;
+        ESP.reset();
+      } else {
+        content = "{\"Error\":\"404 not found\"}";
+        statusCode = 404;
+        Serial.println("Sending 404");
+      }
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(statusCode, "application/json", content);
+ 
+    });
+  } 
+}
